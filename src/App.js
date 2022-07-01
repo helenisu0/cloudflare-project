9c8be757416b3a670adecbc3adf9786dc179fa80
Helen Isu <helenisu0@gmail.com>
2022-07-01 14:21:57 +0100

    solution_one

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Sidebar from './components/sidebar/Sidebar';
 import TopBar from './components/topbar/TopBar'
-import './app.css'
+import './App.css'
 import Home from './pages/home/Home';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import EmployeeList from './pages/userList/EmployeeList';
